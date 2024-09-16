b79975ed3779d75cbd66eb05d572cd57f32fc06d
Robin Bal <balrobin@hotmail.com>
2024-09-16 14:05:42 +0200

    Lagt in components

---
+++ src/components/Login/Login.stories.jsx
@@ -0,0 +1,8 @@
+import Login from "./Login";
+
+export default {
+  title: "frk/components/Login",
+  component: Login,
+};
+
+export const Default = {};
---
+++ src/components/Login/User.stories.jsx
@@ -0,0 +1,8 @@
+import User from "./User";
+
+export default {
+  title: "frk/components/User",
+  component: User,
+};
+
+export const Default = {};
